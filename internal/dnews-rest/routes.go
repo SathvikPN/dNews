070c89d2b774071b6df30ec994f1a7b57842e204
SathvikPN <sathvikapn@gmail.com>
2023-11-25 00:38:05 +0530

    checks error for response writer

--- internal/dnews-rest/routes.go
+++ internal/dnews-rest/routes.go
@@ -1,6 +1,7 @@
 package dnewsrest
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -10,7 +11,11 @@ func (a *application) routes() http.Handler {
 	// http multiplexer
 	router := chi.NewRouter()
 	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Application Name: " + a.appName + "\n"))
+		responseBody := fmt.Sprintln("Application Name:", a.appName)
+		_, err := w.Write([]byte(responseBody))
+		if err != nil {
+			a.errLog.Println("failed to write response body, URI:", r.URL)
+		}
 	})
 	return router
 }
